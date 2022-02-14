6d4a5e2e65a1a5c8f250e3622b52c9777917904c
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-02-14 18:05:22 +1300

    Create ex4_word_A.py

---
+++ ex4_word_A.py
@@ -0,0 +1,17 @@
+""" Word starts with A
+Created by Joseph Wong
+14/02/2022
+"""
+
+
+# Check if word starts with A
+def starts_with_a(word_to_check):
+    if word_to_check.startswith('a') or word_to_check.startswith('A'):
+        return True
+    else:
+        return False
+
+
+# Main Routine
+word = str(input("Enter word: "))
+print(starts_with_a(word))
