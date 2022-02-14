f432ffd3fb4e4c688c8c3b3101520480e881becf
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-02-14 13:40:30 +1300

    Ex 1

---
+++ ex1_add_numbers.py
@@ -0,0 +1,16 @@
+""" Add Numbers
+Created by Joseph Wong
+14/02/2022
+"""
+
+
+# Adding Numbers function
+def add_numbers():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    return num1 + num2
+
+
+# Main Routine
+print(add_numbers())
+print(add_numbers())
